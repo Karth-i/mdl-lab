c8c185871d78e9b9c751d0756898e62b54364092
B Karthik <139115864+Karth-i@users.noreply.github.com>
2023-11-23 23:40:36 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,7 +5,8 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -38,21 +39,18 @@ def transform_text(text):
 
     return " ".join(y)
 
-# Load the pre-trained model and vectorizer
+# Load the pre-trained vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+
+# Use linear SVM with hyperparameter tuning
+param_grid = {'classifier__C': [0.1, 1, 10, 100]}
+svm_model = GridSearchCV(SVC(kernel='linear'), param_grid, cv=3, scoring='accuracy', n_jobs=-1)
 
 # Create an ML pipeline
 pipeline = Pipeline([
     ('tfidf', tfidf),
-    ('classifier', model)
+    ('classifier', svm_model)
 ])
-
-# Initialize session state
-if 'spam_count' not in st.session_state:
-    st.session_state.spam_count = 0
-if 'not_spam_count' not in st.session_state:
-    st.session_state.not_spam_count = 0
 
 st.title("Email/SMS Spam Classifier")
 
@@ -65,14 +63,12 @@ if st.button('Predict'):
     # Display the result
     if result == 1:
         st.header("Spam")
-        st.session_state.spam_count += 1
     else:
         st.header("Not Spam")
-        st.session_state.not_spam_count += 1
 
     # Bar chart
     fig, ax = plt.subplots()
-    ax.bar(['Not Spam', 'Spam'], [st.session_state.not_spam_count, st.session_state.spam_count], color=['blue', 'red'])
+    ax.bar(['Not Spam', 'Spam'], [1 - result, result], color=['blue', 'red'])
     ax.set_ylabel('Count')
     ax.set_title('Distribution of Predictions')
 
